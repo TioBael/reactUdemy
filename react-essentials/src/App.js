ab30640498ada5ce334aafed04e0762e789691d7
TioBael <luis.moreira.123@hotmail.com>
2024-09-27 22:28:40 -0300

    usando Props

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,13 +1,32 @@
 import "./App.css";
+import { CORE_CONCEPTS } from "./data";
 import reactCoreConceptsLogo from "./assets/react-core-concepts.png";
 
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max));
+}
+
+function CoreConcept({img, title, description}) {
+  return (
+    <li>
+      <img src={img} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(reactDescriptions.length)];
+
   return (
     <header>
       <img src={reactCoreConceptsLogo} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
@@ -19,7 +38,18 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((concept, index) => (
+              <CoreConcept key={index} {...concept} />
+            ))}
+          </ul>
+        </section>
+        <h2>
+          <br />
+          Time to get started!
+        </h2>
       </main>
     </div>
   );
